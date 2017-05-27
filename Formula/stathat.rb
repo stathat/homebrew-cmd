16ec19c0de819818edffc8a8978c3e3a40fee682
Patrick Crosby <pcrosby@gmail.com>
2017-05-26 22:07:54 -0500

    Fix sha for 0.2.2

--- Formula/stathat.rb
+++ Formula/stathat.rb
@@ -2,7 +2,7 @@ class Stathat < Formula
   desc "Command-line interface to stathat.com"
   homepage "https://stathat.com"
   url "https://github.com/stathat/cmd/archive/v0.2.2.tar.gz"
-  sha256 "ff790f6b8c2246db30fc9a4c6c7ab1abe2491ed8012fd0925e0197628f268399"
+  sha256 "e8bc6e307f8ccedeb5d576d70891d5229ee6605355ff901cb6804147a5cc65b8"
 
   head "https://github.com/stathat/cmd.git"
 
